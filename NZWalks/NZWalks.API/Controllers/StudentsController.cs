0ca3fb7c2eeea9a3ce26797f7de7da56b310977c
Pham Duy Hung <141256670+Hunggg29@users.noreply.github.com>
2025-02-07 01:49:28 +0700

    Restful API

---
+++ NZWalks/NZWalks.API/Controllers/StudentsController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NZWalks.API.Controllers
+{   
+    //https://localhost:postnumber/api/students 
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        //GET: https://localhost:postnumber/api/students 
+        [HttpGet]
+        public IActionResult GetAllStudents()
+        {
+            string[] studentNames = new string[] { "John", "hung", "Jane", "hehe", "siuuu" };
+            return Ok(studentNames);
+        }
+
+    }
+}
